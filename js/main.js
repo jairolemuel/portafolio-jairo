2aa255b6d8e8a0f67067b18de0499d4a07a4902e
jairolemuel <barrientosjairolemuel@gmail.com>
2025-05-21 17:06:54 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Animación de bienvenida
+setTimeout(() => {
+  const welcome = document.getElementById('welcome-screen');
+  if (welcome) {
+    welcome.style.opacity = '0';
+    setTimeout(() => welcome.remove(), 1000);
+  }
+}, 2500);
+
+// Inicializar AOS (Animate On Scroll)
+AOS.init({
+  duration: 1000,
+  once: true,
+});
